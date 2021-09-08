c69a186dc6a2b8c7d37579e39824da87135f9ad3
Daniel <danielselga@gmail.com>
2021-09-08 00:21:28 -0300

    chore: then and catch

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const superagent = require('superagent')
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     console.log(`Breed: ${data}`)
 
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
-        if(err) console.log(err.message);
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).then((res) => { //then expect a response.
         console.log(res.body.message)
    
         fs.writeFile('dog-image.txt', res.body.message, err => {
         console.log('random dog image to file')
-        })
     })
+  }).catch(err => { //Catch will triggered if catch isnt work
+      console.log(err.message)
+  })
 })
