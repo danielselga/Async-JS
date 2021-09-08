b4c03c97785d201017a498d9fe0d4b475c0dd92a
Daniel <danielselga@gmail.com>
2021-09-08 00:30:16 -0300

    chore: creating a promisse function

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const fs = require('fs')
 const superagent = require('superagent')
 
+//Creating a promise
+const readFilePro = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if(err) {
+                reject('I coldnt read this file')
+                resolve(data)
+            }
+        })
+    })
+}
+
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     console.log(`Breed: ${data}`)
 
